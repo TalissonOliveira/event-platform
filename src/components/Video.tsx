386f90fa82d0cae156f4b77ecbc1269028ee080d
Talisson Oliveira <talissonlux@outlook.com>
2022-06-23 17:48:35 -0300

    feat: adds video player with youtube provider

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,11 +1,19 @@
+import { DefaultUi, Player, Youtube } from '@vime/react'
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from 'phosphor-react'
 import { Button } from './Button'
+
+import '@vime/core/themes/default.css'
 
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="w-full h-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="w-full h-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId={'SO4-izct7Mc'} />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
